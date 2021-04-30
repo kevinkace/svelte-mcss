07b75aa784e0eb32d716ca1b9f79f4a05bbf2725
Kevin Cameron <kevinacameron@hotmail.com>
2021-04-29 17:17:06 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Comp1 from './comp1/Comp1.svelte';
+	import css from './main.css';
+
+	export let name;
+
+	console.log(css);
+</script>
+
+<main>
+	<h1 class={css.header}>Hello {name}!</h1>
+
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<Comp1 />
+</main>
+
+<style>
+</style>
---
+++ src/comp1/Comp1.svelte
@@ -0,0 +1,9 @@
+<script>
+	import css from './comp1.css';
+
+	console.log(css);
+</script>
+
+<dev>
+	<h1 class={css.comp1}>Hello comp1!</h1>
+</dev>
