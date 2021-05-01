697654cc3557de5e206e9de31ab6a9f860864032
Kevin Cameron <kevinacameron@hotmail.com>
2021-05-01 12:15:15 -0700

    cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,17 @@
 <script>
 	import css from './main.css';
 
-	export let name;
-
-	console.log(typeof css);
-	console.log(css);
-	console.log(css.header);
-	console.log(css["header"]);
+	console.log(typeof css); // object
+	console.log(css); // {header: "mca712d7b4_header"}
+	console.log(css["header"]); // mca712d7b4_header
+	console.log(css.header); // css.header
 </script>
 
 <main>
-	<h1 class={css.header}>class={css.header}</h1>
-	<h1 class={css["header"]}>class={css["header"]}</h1>
-	<p>both should have class "header" and be green</p>
+	<h1 class={css.header}>class={'{css.header}'} - {css.header}</h1>
+	<h1 class={css["header"]}>class={'{css["header"]}'} - {css["header"]}</h1>
+
+	<p>both should have class "mXXX_header" and be green</p>
 </main>
 
 <style>
